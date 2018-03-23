fdac3a72a3f1963767b385561a30d2d8960086bf
Brian Hopkins <brianhh1230@gmail.com>
2018-03-23 13:00:33 -0400

    testing pair my device

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 /mycroft/ai/./start-mycroft.sh all
+sleep 30s
+python /mycroft/ai/mycroft/messagebus/send.py speak "'{"utterance": "pair my device"}'"
 tail -f /mycroft/ai/scripts/logs/mycroft-skills.log
