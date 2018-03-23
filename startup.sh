d96cb8023e53244c38157bf54953fad873f4c56e
Brian Hopkins <brianhh1230@gmail.com>
2018-03-23 13:20:36 -0400

    updating locations

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/mycroft/ai/./start-mycroft.sh all
-tail -f /mycroft/ai/scripts/logs/mycroft-skills.log
+/opt/mycroft/./start-mycroft.sh all
+tail -f /opt/mycroft/scripts/logs/mycroft-skills.log
